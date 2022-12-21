0d1cb4092f2d8b98c40de9087e1f890389fe88f4
alissonpratesperes <a.pratesperes@gmail.com>
2022-12-21 17:09:02 -0300

    🔧 Implementação de nova Migration: "03_create_connections", no Back-End da Aplicação.
    Implementada nova Migration: "03_create_connections", para a Criação da Tabela "connections" e suas respectivas Colunas no Banco de Dados. Tabela essa que receberá as Conexões que serão realizadas com os Professores (e que relacionarão aos IDs dos Professores usando as Foreign Keys no KnexJS) bem como o "timestamp" de quando foram criadas na Plataforma, no Back-End da Aplicação.

---
+++ backend/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,15 @@
+import { Knex } from "knex";
+
+    export async function up(knex: Knex) {
+        return knex.schema.createTable("connections", table => {
+            table.increments("id").primary();
+
+            table.timestamp("created_at").notNullable().defaultTo("now()");
+
+            table.integer("teacher_id").notNullable().references("id").inTable("teachers").onUpdate("CASCADE").onDelete("CASCADE");
+        });
+    };
+
+        export async function down(knex: Knex) {
+            return knex.schema.dropTable("connections");
+        };
