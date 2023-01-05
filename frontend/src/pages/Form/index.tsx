bf6bee048e56226634f709243f501f0e64624625
alissonpratesperes <a.pratesperes@gmail.com>
2023-01-05 00:51:41 -0300

    :sparkles: Novas implementações do Component: "Select", no Front-End da Aplicação.
    Criado novo Fieldset na Página - Form (Cadastro de Proffys), e implementado o Component: "Select" para os "Dias da semana" e "Horários - Das, Até", bem como a Estilização CSS Proprietária, sem fugir muito do Escopo do Curso mas seguindo o Conceito de Mobile First (responsividade para Mobile);
    Iniciada utilizando Mocking, a implementação dos Estados para os testes iniciais nesse novo Fieldset implementado, no Front-End da Aplicação.

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "../../components/Header";
 import Input from "../../components/Input";
@@ -6,54 +6,92 @@ import Textarea from "../../components/Textarea";
 import Select from "../../components/Select";
 import warning from "../../assets/icons/warning.svg";
 import heart from "../../assets/icons/purple-heart.svg";
+import rocket from "../../assets/icons/rocket.svg";
 import "./styles.css";
 
     function Form() {
-        return (
-            <div id="page_form" className="container">
-                <Header title="Que incrível que você quer dar aulas" description="O primeiro passo é preencher esse formulário de inscrição"/>
-                    <main>
-                        <fieldset>
-                            <legend>
-                                Seus dados
-                            </legend>
-                                <Input label="Nome" name="name"/>
-                                <Input label="Avatar" name="avatar"/>
-                                <Input label="WhatsApp" name="whatsapp"/>
-                                    <Textarea label="Biografia" name="biography"/>
-                        </fieldset>
-                        <fieldset>
-                            <legend>
-                                Sobre a aula
-                            </legend>
-                                <Select label="Matéria" name="matter" options={[
-                                    { value: "Artes", label: "Artes" },
-                                    { value: "Biologia", label: "Biologia" },
-                                    { value: "Ciências", label: "Ciências" },
-                                    { value: "Educação Física", label: "Educação Física" },
-                                    { value: "Física", label: "Física" },
-                                    { value: "Geografia", label: "Geografia" },
-                                    { value: "História", label: "História" },
-                                    { value: "Matemática", label: "Matemática" },
-                                    { value: "Português", label: "Português" },
-                                    { value: "Química", label: "Química" }
-                                ]}/>
-                                    <Input label="Custo Hora/Aula" name="cost"/>
-                        </fieldset>
-                            <footer>
-                                <p>
-                                    <img src={ warning } alt="Warning"/>
-                                        Importante! <br/>
-                                        Preencha todos os dados
-                                </p>
-                                    <button type="button">
-                                        <img src={ heart } alt="Heart"/>
-                                            Salvar cadastro
-                                    </button>
-                            </footer>
-                    </main>
-            </div>
-        );
+        const [ scheduleItems, setScheduleItems ] = useState([
+            { week_day: 0, from: "", to: "" }
+        ]);
+
+            function addNewScheduleItem() {
+                setScheduleItems([
+                    ...scheduleItems,
+                        { week_day: 0, from: "", to: "" }
+                ]);
+            };
+
+            return (
+                <div id="page_form" className="container">
+                    <Header title="Que incrível que você quer dar aulas" description="O primeiro passo é preencher esse formulário de inscrição"/>
+                        <main>
+                            <fieldset>
+                                <legend>
+                                    Seus dados
+                                </legend>
+                                    <Input label="Nome" name="name"/>
+                                    <Input label="Avatar" name="avatar"/>
+                                    <Input label="WhatsApp" name="whatsapp"/>
+                                        <Textarea label="Biografia" name="biography"/>
+                            </fieldset>
+                            <fieldset>
+                                <legend>
+                                    Sobre a aula
+                                </legend>
+                                    <Select label="Matéria" name="matter" options={[
+                                        { value: "Artes", label: "Artes" },
+                                        { value: "Biologia", label: "Biologia" },
+                                        { value: "Ciências", label: "Ciências" },
+                                        { value: "Educação Física", label: "Educação Física" },
+                                        { value: "Física", label: "Física" },
+                                        { value: "Geografia", label: "Geografia" },
+                                        { value: "História", label: "História" },
+                                        { value: "Matemática", label: "Matemática" },
+                                        { value: "Português", label: "Português" },
+                                        { value: "Química", label: "Química" }
+                                    ]}/>
+                                        <Input label="Custo Hora/Aula" name="cost"/>
+                            </fieldset>
+                            <fieldset>
+                                <legend>
+                                    Horários disponíveis
+                                        <button type="button" onClick={ addNewScheduleItem }>
+                                            <img src={ rocket } alt="Rocket"/>
+                                                Novo horário
+                                        </button>
+                                </legend>
+                                    { scheduleItems.map(scheduleItem => {
+                                        return (
+                                            <div key={ scheduleItem.week_day } className="schedule-item">
+                                                <Select label="Dia da semana" name="week_day" options={[
+                                                    { value: "0", label: "Domingo" },
+                                                    { value: "1", label: "Segunda-feira" },
+                                                    { value: "2", label: "Terça-feira" },
+                                                    { value: "3", label: "Quarta-feira" },
+                                                    { value: "4", label: "Quinta-feira" },
+                                                    { value: "5", label: "Sexta-feira" },
+                                                    { value: "6", label: "Sábado" }
+                                                ]}/>
+                                                    <Input label="Das" name="from" type="time"/>
+                                                    <Input label="Até" name="to" type="time"/>
+                                            </div>
+                                        );
+                                    }) }
+                            </fieldset>
+                                <footer>
+                                    <p>
+                                        <img src={ warning } alt="Warning"/>
+                                            Importante! <br/>
+                                            Preencha todos os dados
+                                    </p>
+                                        <button type="button">
+                                            <img src={ heart } alt="Heart"/>
+                                                Salvar cadastro
+                                        </button>
+                                </footer>
+                        </main>
+                </div>
+            );
     };
 
         export default Form;
