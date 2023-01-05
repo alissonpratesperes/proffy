7471c747f19312b01badb5d36e341ad640665513
alissonpratesperes <a.pratesperes@gmail.com>
2023-01-05 03:02:25 -0300

    👽 Integração da Página: "Landing" com a API, no Front-End da Aplicação.
    Implementada a Integração da Página: "Landing" com a API, utilizando o Método ".get" do Axios para o recurso apontado, nesse caso "/connections" - Para receber o Total de Conexões Realizadas, com a utilização também de uma Promise que retorna a variável "response", onde é feita a desestruturação e coletado o "total" de "response.data" e atualizado o Estado: "totalConnections" através da Função: "setTotalConnections" recebendo o "total" como parâmetro, no Front-End da Aplicação.

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/logo.svg";
@@ -6,34 +6,48 @@ import landing from "../../assets/images/landing.svg";
 import study from "../../assets/icons/study.svg";
 import classes from "../../assets/icons/give-classes.svg";
 import heart from "../../assets/icons/purple-heart.svg";
+import api from "../../services/api";
 import "./styles.css";
 
     function Landing() {
-        return (
-            <div id="page_landing">
-                <div id="page_landing_content" className="container">
-                    <div className="container-logo">
-                        <img src={ logo } alt="Proffy"/>
-                            <h2> Sua plataforma de estudos on-line </h2>
-                    </div>
-                        <img src={ landing } alt="Landing" className="landing-image"/>
-                            <div className="container-buttons">
-                                <Link className="study-button" to="/studies">
-                                    <img src={ study } alt="Studies"/>
-                                        Estudar
-                                </Link>
-                                <Link className="class-button" to="/classes">
-                                    <img src={ classes } alt="Classes"/>
-                                        Lecionar
-                                </Link>
+        const [ totalConnections, setTotalConnections ] = useState(0); 
+
+            useEffect(() => {
+                api.get(
+                    "/connections"
+                ).then(
+                    response => {
+                        const { total } = response.data;
+                            setTotalConnections(total);
+                    }
+                );
+            }, []);
+
+                return (
+                    <div id="page_landing">
+                        <div id="page_landing_content" className="container">
+                            <div className="container-logo">
+                                <img src={ logo } alt="Proffy"/>
+                                    <h2> Sua plataforma de estudos on-line </h2>
                             </div>
-                                <span className="total-connections">
-                                    Total de 200 conexões já realizadas
-                                        <img src={ heart } alt="Heart"/>
-                                </span>
-                </div>
-            </div>
-        );
+                                <img src={ landing } alt="Landing" className="landing-image"/>
+                                    <div className="container-buttons">
+                                        <Link className="study-button" to="/studies">
+                                            <img src={ study } alt="Studies"/>
+                                                Estudar
+                                        </Link>
+                                        <Link className="class-button" to="/classes">
+                                            <img src={ classes } alt="Classes"/>
+                                                Lecionar
+                                        </Link>
+                                    </div>
+                                        <span className="total-connections">
+                                            Total de { totalConnections } conexões já realizadas
+                                                <img src={ heart } alt="Heart"/>
+                                        </span>
+                        </div>
+                    </div>
+                );
     };
 
         export default Landing;
